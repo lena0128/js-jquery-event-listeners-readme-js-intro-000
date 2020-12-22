29c35a1a9ba093c27835bd15bf37e7254149925a
Lena Hou <lenahou2013@gmail.com>
2020-12-22 22:20:30 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,13 +1,12 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function(){
+    alert("Hey!");
+  });
+}
+
 
 $(document).ready(function(){
 // call functions here
-$("form").on("submit", function() {
-  if ($( "input:first" ).val() === "correct") {
-    alert('your form is going to be submitted now');
-    return;
-  }
-  alert("you entered the wrong value");
-  return;
-});
+getIt();
 });
